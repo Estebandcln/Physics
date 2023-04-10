1678da4a70233cfd0e55e5b68d218ecd58b8f183
Esteban J. H. Décline <72504779+Estebandcln@users.noreply.github.com>
2023-04-10 02:20:13 +0200

    Add files via upload

--- Heat_transfer.py
+++ Heat_transfer.py
@@ -11,15 +11,16 @@ import numpy as np
 # N-parts wall
 
 T_i = 20  # Internal environment temperature (°C)
-T_f = 15  # External environment temperature (°C)
-S = 10  # Wall area (m²)
+T_f = 0  # External environment temperature (°C)
+S = 100  # Wall area (m²)
 
 # Materials thermal conductivity lambda
 
 l_Cu = 398 #W/(m.°C)
 l_Pb = 34.3 #W/(m.°C)
 l_Al = 226 #W/(m.°C)
-l_SS = 13.4 #W/(m.°C)
+l_Wf = 0.04 #W/(m.°C)
+l_Gw = 0.036 #W/(m.°C)
 l_gl = 1.05 #W/(m.°C)
 l_oak = 0.16 #W/(m.°C)
 
@@ -32,34 +33,35 @@ h_water = 75 #W/(m.°C)
 
 e_Cu = 1 #m
 e_Pb = 0.5 #m
-e_Al = 0.1 #m
-e_SS = 0.6 #m
-e_gl = 0.4 #m
-e_oak = 1 #m
+e_Al = 1 #m
+e_Wf = 0.02 #m
+e_Gw = 0.02 #m 
+e_gl = 0.01 #m
+e_oak = 0.1 #m
 e_air = 0.5 #m
 e_water = 0.5 #m
 
 # Wall composition
 
 f = ['Air', 'Water'] # Fluids
-m = ['Air', 'Water', 'Copper', 'Lead', 'Aluminum', 'Stained steel', 'Glass', 'Oak'] # Layers materials
-wall = ['Water', 'Lead', 'Aluminum', 'Lead', 'Copper', 'Air']
-N = []
+m = ['Air', 'Water', 'Copper', 'Lead', 'Aluminum', 'Wood fiber', 'Glass wool', 'Glass', 'Oak'] # Layers materials
+wall = ['Air', 'Aluminum', 'Wood fiber', 'Copper', 'Glass wool', 'Water'] # Input you wall here
+N1= [] # Ordered materials and fluids from the inside to the outside
 
 for i in wall:
     for j in range(len(m)):
         if i == m[j]:
-            N.append(j)
+            N1.append(j)
 
-coeff = [h_air, h_water, l_Cu, l_Pb, l_Al, l_SS, l_gl, l_oak]
-e = [e_air, e_water, e_Cu, e_Pb, e_Al, e_SS, e_gl, e_oak]
+coeff = [h_air, h_water, l_Cu, l_Pb, l_Al, l_Wf, l_Gw, l_gl, l_oak]
+e = [e_air, e_water, e_Cu, e_Pb, e_Al, e_Wf, e_Gw, e_gl, e_oak]
 
 # Thermal resistance
 
 R_th = []
 R_tot = 0 # °C/W
 
-for i in N:
+for i in N1:
     if m[i] in f:
         
         R_th.append(1/(coeff[i]*S)) # Fluid = convection
@@ -70,81 +72,218 @@ for i in N:
         R_th.append(e[i]/(coeff[i]*S)) # Material = conduction
         R_tot += e[i]/(coeff[i]*S)
 
-phi = np.abs((T_i-T_f))/(R_tot)
+phi = np.abs(round(((T_i-T_f))/(R_tot),4))
 
-T_list = [T_i] # Internal environment, internal surface, n layers, external surface, external environment
+T_list1 = [T_i] # Internal environment, internal surface, n layers, external surface, external environment
 a = 0
 
 if T_i>T_f:    
-    for i in N:
+    for i in N1:
         if m[i] in f:
-            if len(T_list)<2:
+            if len(T_list1)<2:
 
-                T_list.append(T_i-phi/(S*coeff[0]))
-                print(T_i,coeff[0],T_i-phi/(S*coeff[0]),a,i)
+                T_list1.append(round(T_i-phi/(S*coeff[i]),5))
+                print(T_i,coeff[i],T_i-phi/(S*coeff[i]),a,i)
                 a += 1
                 
             else:
                
-                T_list.append(T_f+phi/(S*coeff[1]))
-                print(T_f,coeff[1],T_f+phi/(S*coeff[1]),a,i)
+                T_list1.append(round(T_f+phi/(S*coeff[i]),5))
+                print(T_f,coeff[i],T_f+phi/(S*coeff[i]),a,i)
                 a += 1
                 
         else:
-            T_list.append(T_list[a]-phi*e[i]/(S*coeff[i]))
-            print(T_list[a],e[i],coeff[i],T_list[a]-phi*e[i]/(S*coeff[i]),a,i)
+            T_list1.append(round(T_list1[a]-phi*e[i]/(S*coeff[i]),5))
+            print(T_list1[a],e[i],coeff[i],T_list1[a]-phi*e[i]/(S*coeff[i]),a,i)
             a += 1
 else:
 
-    for i in N:
+    for i in N1:
         if i in f:
-            if len(T_list)<2:
+            if len(T_list1)<2:
 
-                T_list.append(T_i+phi/(S*coeff[0]))
-                print(T_i,coeff[0],T_i-phi/(S*coeff[0]),a,i)
+                T_list1.append(T_i+phi/(S*coeff[i]))
+                print(T_i,coeff[i],T_i-phi/(S*coeff[i]),a,i)
                 a += 1
                 
             else:
                
-                T_list.append(T_f-phi/(S*coeff[1]))
-                print(T_f,coeff[1],T_f+phi/(S*coeff[1]),a,i)
+                T_list1.append(T_f-phi/(S*coeff[i]))
+                print(T_f,coeff[i],T_f+phi/(S*coeff[i]),a,i)
                 a += 1
                 
         else:
-            T_list.append(T_list[a]+phi*e[i]/(S*coeff[i]))
-            print(T_list[a],e[i],coeff[i],T_list[a]-phi*e[i]/(S*coeff[i]),a,i)
+            T_list1.append(T_list1[a]+phi*e[i]/(S*coeff[i]))
+            print(T_list1[a],e[i],coeff[i],T_list1[a]-phi*e[i]/(S*coeff[i]),a,i)
             a += 1
             
-k1 = T_list[-1]
-print(T_list)
-T_list.remove(k1)
-print(T_list)
-k2 = (k1+T_list[-1])/2
-T_list.remove(T_list[-1])
-print(T_list)
-T_list.append(k2)
-print(T_list)
-T_list.append(T_f)
-print(T_list)
+k1 = T_list1[-1]
+print(T_list1)
+T_list1.remove(k1)
+print(T_list1)
+k2 = (k1+T_list1[-1])/2
+T_list1.remove(T_list1[-1])
+print(T_list1)
+T_list1.append(k2)
+print(T_list1)
+T_list1.append(T_f)
+print(T_list1)
 
-x_list = [0]
+x_list1 = [0]
+b = 0
+
+for i in N1:
+    x_list1.append(e[i]+x_list1[b])
+    b += 1
+
+x_max = x_list1[-1]
+
+plt.figure(1)
+
+#x = np.linspace(0, x_max, len(T_list))
+plt.plot(x_list1, T_list1, label = 'Temperature', c='r')
+plt.legend()
+plt.title('Temperature profile')
+plt.xlabel('Wall thickness (m)')
+plt.ylabel('Temperature within the wall (°C)')
+
+del x_list1[0]
+del x_list1[-1]
+for i in x_list1:
+    
+    plt.axvline(x=i, c='k', linewidth=1)
+    
+    
+
+############################## From the outside #############################
+print('méthode 2')
+N2 = []
+N = N1.copy()
+for i in range(len(N1)):
+    N2.append(N1[-1])
+    del N1[-1] 
+    
+T_list2 = [T_f] # External environment, external surface, n layers, internal surface, internal environment
+a = 0
+
+if T_i>T_f:    
+    for i in N2:
+        if m[i] in f:
+            if len(T_list2)<2:
+
+                T_list2.append(round(T_f+phi/(S*coeff[i]),5))
+                print(T_f,coeff[i],T_f+phi/(S*coeff[i]),a,i)
+                a += 1
+                
+            else:
+               
+                T_list2.append(round(T_i-phi/(S*coeff[i]),5))
+                print(T_i,coeff[i],T_i-phi/(S*coeff[i]),a,i)
+                a += 1
+                
+        else:
+            T_list2.append(round(T_list2[a]+phi*e[i]/(S*coeff[i]),5))
+            print(T_list2[a],e[i],coeff[i],T_list2[a]+phi*e[i]/(S*coeff[i]),a,i)
+            a += 1
+else:
+
+    for i in N2:
+        if i in f:
+            if len(T_list1)<2:
+
+                T_list2.append(T_i+phi/(S*coeff[i]))
+                print(T_i,coeff[i],T_i-phi/(S*coeff[i]),a,i)
+                a += 1
+                
+            else:
+               
+                T_list2.append(T_f-phi/(S*coeff[i]))
+                print(T_f,coeff[i],T_f+phi/(S*coeff[i]),a,i)
+                a += 1
+                
+        else:
+            T_list2.append(T_list2[a]+phi*e[i]/(S*coeff[i]))
+            print(T_list2[a],e[i],coeff[i],T_list2[a]-phi*e[i]/(S*coeff[i]),a,i)
+            a += 1
+            
+k1 = T_list2[-1]
+print(T_list2)
+T_list2.remove(k1)
+print(T_list2)
+k2 = (k1+T_list2[-1])/2
+T_list2.remove(T_list2[-1])
+print(T_list2)
+T_list2.append(k2)
+print(T_list2)
+T_list2.append(T_i)
+print(T_list2)
+
+x_list2 = [0]
 b = 0
 
 for i in N:
-    x_list.append(e[i]+x_list[b])
+    x_list2.append(e[i]+x_list2[b])
+    print(e[i]+x_list2[b])
     b += 1
 
-x_max = x_list[-1]
+T_list2.reverse()
+x_max = x_list2[-1]
 
+plt.figure(2)
 #x = np.linspace(0, x_max, len(T_list))
+plt.plot(x_list2, T_list2, label = 'Temperature', c='r')
+plt.legend()
+plt.title('Temperature profile')
+plt.xlabel('Wall thickness (m)')
+plt.ylabel('Temperature within the wall (°C)')
+
+del x_list2[0]
+del x_list2[-1]
+for i in x_list2:
+
+    plt.axvline(x=i, c='k', linewidth=1)
+
+# Mean value? List of the converged values
+
+x_list = [0]
+c = 0
+
+for i in N:
+    x_list.append(e[i]+x_list[c])
+    print(e[i]+x_list[c])
+    c += 1
+
+a = 0
+b = 0
+h = 1e-3
+T_list = []
+for i in range(len(T_list1)):
+    if np.abs(round(T_list1[i]-T_list2[i],5))<0.1:
+        a += 1
+        T_list.append(T_list1[i])
+    else:
+        b += 1
+        T = np.abs(T_list1[i]-T_list2[i])
+        print("T",T)
+        while T>0.1:
+            T_list1[i] = round(T_list1[i]*(1+h),5)
+            T_list2[i] = round(T_list2[i]*(1+h),5)
+            T = np.abs(round(T_list1[i]-T_list2[i],5))
+
+        T_list.append(T)
+
+plt.figure(3)
+
 plt.plot(x_list, T_list, label = 'Temperature', c='r')
 plt.legend()
 plt.title('Temperature profile')
 plt.xlabel('Wall thickness (m)')
 plt.ylabel('Temperature within the wall (°C)')
 
-x_list.remove(x_list[0])
-x_list.remove(x_list[-1])
+print(x_list)
+del x_list[0]
+del x_list[-1]
+print(x_list)
 for i in x_list:
     
     plt.axvline(x=i, c='k', linewidth=1)
